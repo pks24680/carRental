7148e552b9c2063a6a9c445c165e1bba8dd27953
Pruthvi Shah <113222650+pks24680@users.noreply.github.com>
2024-06-04 20:10:58 +1000

    Add files via upload

---
+++ about.php
@@ -0,0 +1,34 @@
+<?php include('layouts/header.php'); ?>
+
+<section id="about" class="pt-5">
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-md-8 mx-auto">
+                <h1>Why Choose Us?</h1>
+                <p>Here are some reasons why you should shop with us:</p>
+                <ul>
+                    <li>We offer a wide variety of cars.</li>
+                    <li>Convenient online reservation.</li>
+                    <li>High-quality cars and brands sourced from trusted suppliers.</li>
+                    <li>Competitive prices and great deals.</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-md-8 mx-auto">
+                <h1>Our Mission</h1>
+                <p>At our car rental store, our mission is to:</p>
+                <ul>
+                    <li>Provide customers with the easiest rental experience.</li>
+                    <li>Make renting cars convenient and hassle-free.</li>
+                    <li>Support upcoming brands.</li>
+                    <li>Deliver exceptional customer service.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
